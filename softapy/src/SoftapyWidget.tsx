433774904d72e674c20f77d18ddb0e69d0582bce
justege <okuregemen1@gmail.com>
2023-06-20 19:48:29 +0300

    working frontend with two customer

--- softapy/src/SoftapyWidget.tsx
+++ softapy/src/SoftapyWidget.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 type ChatGPT = {
   id: number;
+  chatWebsiteURL: string;
   requestId: number;
   inputChatGPT: string;
   outputChatGPT: string;
@@ -92,41 +93,37 @@ interface Popup {
   popupCTAButtonLink: string | null;
   popupCTAButtonBorderColor: string | null;
   popupCTAButtonHasBorder: boolean | null;
-	popupTitleAndContentPercentage : number | null;
-	popupChatHistoryPercentage: number | null;
-	popupChatSendPercentage: number | null;
-	popupCTAPercentage : number | null;
+  popupSendButtonColor: string | null;
+  popupSendButtonTextColor: string | null;
+  popupSendButtonText: string | null;
+  popupSendButtonVariant: string | null;
+  popupSendButtonColorScheme: string | null;
+	popupTitleAndContentPercentage : string | null;
+	popupChatHistoryPercentage: string | null;
+	popupChatSendPercentage: string | null;
+	popupCTAPercentage : string | null;
+  popupExampleInputChatGPT: string | null;
+  popupExampleOutputChatGPT: string | null;  
 }
-
-
-
 
 
 function SoftapyWidget({ widget }: Props) {
   const [popupEngagement, setPopupEngagement] = useState<PopupEngagement>()
   const [popupAdditionals, setPopupAdditionals] = useState<PopupAdditional[]>([])
   const [popup, setPopup] = useState<Popup>()
-  const [websiteUrl, setWebsiteUrl] = useState<string | null>(null);
   const [cacheKeys, setCacheKeys] = useState<string[]>([]);
   const [notFound, setNotFound] = useState<boolean>();
   const [changed, setChanged] = useState(false);
   const [error, setError] = useState<unknown>();
-  const id = parseInt(widget.getAttribute('userId') ?? '1');
-  const popupId = parseInt(widget.getAttribute('popupId') ?? '1');
+  const id = parseInt(widget.getAttribute('userId') ?? '0');
+  const popupId = parseInt(widget.getAttribute('popupId') ?? '0');
   const [chatGPTs, setChatGPTs] = useState<ChatGPT[]>([]);
   const [inputChatGPT, setInputChatGPT] = useState('');
   const [pastChatGPTInput, setPastChatGPTInput] = useState<string[]>([]);
 
-  
-  // 1. Create new engagement popup  - Done
-  // 2. Request the Id of the popup  - Done
-  // 3. Request the initial popup title and content 
-  // 4. Prepare for chatinput and send input 
-  // 5. Get chatouput 
-
   const createNewPopupEngagement = async () => {
     try {
-      const response = await fetch(`${baseUrl}popup/createNewPopupEngagement/${id}`, {
+      const response = await fetch(`${baseUrl}popup/createNewPopupEngagement/${popupId}/${id}`, {
         method: 'POST',
       });
       if (!response.ok) {
@@ -142,12 +139,12 @@ function SoftapyWidget({ widget }: Props) {
 
   const chatGPTInput = async (inputChatGPT: string) => {
     try {
-      const response = await fetch(`${baseUrl}popup/chatgpt/${id}/${popupEngagement?.popupEngagementUniqueIdentifier}`, {
+      const response = await fetch(`${baseUrl}popup/chatgpt/${id}/${popupId}/${popupEngagement?.popupEngagementUniqueIdentifier}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ inputChatGPT }),
+        body: JSON.stringify({ inputChatGPT: inputChatGPT, chatWebsiteURL: window.location.href }),
       });
       if (!response.ok) {
         throw new Error('Something went wrong, try again later');
@@ -160,20 +157,6 @@ function SoftapyWidget({ widget }: Props) {
   };
 
   const [popupImage, setPopupImage] = useState<string | null>(null);
-
-  const fecthPopupAdditional  = async () => {
-    try {
-      const response = await fetch(`${baseUrl}popup/${popupId}`);
-      if (!response.ok) {
-        throw new Error('Something went wrong, try again later');
-      }
-      const data = await response.json();
-      setError(undefined);
-    } catch (error) {
-      setError(error);
-    }
-  };
-
 
   const fetchImage = async (imageUrl: string | null) => {
     if (imageUrl) {
@@ -194,7 +177,7 @@ function SoftapyWidget({ widget }: Props) {
 
   const fetchPopup = async () => {
     try {
-      const response = await fetch(`${baseUrl}popup/${popupId}`);
+      const response = await fetch(`${baseUrl}popup/${popupId}/${id}`);
       if (!response.ok) {
         throw new Error('Something went wrong, try again later');
       }
@@ -207,15 +190,20 @@ function SoftapyWidget({ widget }: Props) {
       setError(error);
     }
   };
+
   const fetchChatGPTs = async () => {
     try {
-      const response = await fetch(`${baseUrl}popup/chatgpt/${id}/${popupEngagement?.popupEngagementUniqueIdentifier}`);
+      const response = await fetch(`${baseUrl}popup/chatgpt/${id}/${popupId}/${popupEngagement?.popupEngagementUniqueIdentifier}`);
       if (!response.ok) {
         throw new Error('Something went wrong, try again later');
       }
       const data = await response.json();
-      setPopupAdditionals(data.popupAdditional);
-      setChatGPTs(data.chatgpt);
+      setPopupAdditionals(data.popupAdditional)
+
+      if (data.chatgpt.length) {
+        setChatGPTs(data.chatgpt);
+      } 
+      
       setError(undefined);
     } catch (error) {
       setError(error);
@@ -225,6 +213,7 @@ function SoftapyWidget({ widget }: Props) {
   useEffect(() => {
     fetchPopup();
     fetchChatGPTs();
+
   }, [id,popupEngagement?.popupEngagementUniqueIdentifier,pastChatGPTInput]);
 
  
@@ -266,7 +255,6 @@ function SoftapyWidget({ widget }: Props) {
   useEffect(()=>{
     if (popupCreationState){
       createNewPopupEngagement()
-      console.log(popup)
     }
   },[popupCreationState])
 
@@ -278,192 +266,218 @@ function SoftapyWidget({ widget }: Props) {
     }
   }, [popupEngagement, chatGPTs]);
   
+
+
+
   return (
 <>
-  {popupEngagement ? (
-    <Flex
-      direction="row"
-      w={popup?.popupWidth ?? [800, 550]}
-      h={popup?.popupHeight ?? [500, 350]}
-      bg={popup?.popupBackgroundColor ?? "white"}
-      p={4}
-      position="fixed"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      border={popup?.popupBorderWidth ?? undefined}
-      borderColor={popup?.popupBorderColor ?? undefined}
-      borderRadius="md"
-      boxShadow="lg"
-    >
+{popupEngagement && (
+  <Flex
+    direction="row"
+    w={popup?.popupWidth ?? [800, 550]}
+    h={popup?.popupHeight ?? [500, 350]}
+    bg={popup?.popupBackgroundColor ?? "white"}
+    p={4}
+    position="fixed"
+    top="50%"
+    left="50%"
+    transform="translate(-50%, -50%)"
+    border={popup?.popupBorderWidth ?? undefined}
+    borderColor={popup?.popupBorderColor ?? undefined}
+    borderRadius="md"
+    boxShadow="lg"
+  >
     {popupImage && (
-      <Box 
-      w={"50%"} 
-      >
-        <Img  
-        border={popup?.popupImageBorderWidth ?? undefined}     
-        borderColor={popup?.popupImageBorderColor ?? undefined}
-        width={popup?.popupImageWidth ?? undefined} 
-        height={popup?.popupImageHeight ?? undefined} 
-        src={popupImage}   
-        alt="Popup Image" />
+      <Box w="50%">
+        <Img
+          border={popup?.popupImageBorderWidth ?? undefined}
+          borderColor={popup?.popupImageBorderColor ?? undefined}
+          width={popup?.popupImageWidth ?? undefined}
+          height={popup?.popupImageHeight ?? undefined}
+          src={popupImage}
+          alt="Popup Image"
+        />
       </Box>
     )}
-      <Spacer />
-      <Box w={"50%"} > {/* Wrap the content in a container with overflow hidden */}
 
+    <Spacer />
+
+    <Box w="50%">
       <Button
-          onClick={() => setPopupEngagement(undefined)} // Add an onClick event handler to close the popup
-          bg = {popup?.popupCloseButtonBoxColor ?? undefined}
-          color = {popup?.popupCloseButtonTextColor ?? undefined}
-          colorScheme= {popup?.popupCloseButtonVariantBoxColor ?? undefined}
-          variant = {popup?.popupCloseButtonVariant ?? undefined}
-          position="absolute"
-          top={0}
-          right={0}
-          m={1}
-          p={1}
+        onClick={() => setPopupEngagement(undefined)}
+        bg={popup?.popupCloseButtonBoxColor ?? undefined}
+        color={popup?.popupCloseButtonTextColor ?? undefined}
+        colorScheme={popup?.popupCloseButtonVariantBoxColor ?? undefined}
+        variant={popup?.popupCloseButtonVariant ?? undefined}
+        position="absolute"
+        top={0}
+        right={0}
+        m={1}
+        p={1}
+      >
+        {popup?.popupCloseButtonText}
+      </Button>
+
+      {popupEngagement?.popupEngagementUniqueIdentifier && (
+        <Flex
+          direction="column"
+          h={popup?.popupTitleAndContentPercentage ?? undefined }
+          ml={popup?.popupTextMarginLeft ?? undefined}
+          mr={popup?.popupTextMarginRight ?? undefined}
         >
-          {popup?.popupCloseButtonText}
-        </Button>
+          <Box>
+            {popup?.popupTitle && (
+              <Box
+                p={2}
+                mt={2}
+                minHeight={popup?.popupTitleHeight ?? undefined}
+                width={popup?.popupTitleWidth ?? undefined}
+                textColor={popup?.popupTitleTextColor ?? undefined}
+              >
+                <Text fontSize="3xl" textAlign="center">
+                  {popup?.popupTitle}
+                </Text>
+              </Box>
+            )}
 
-        {popupEngagement?.popupEngagementUniqueIdentifier && (
-          <Flex direction={"column"} h="40%" ml={popup?.popupTextMarginLeft ?? undefined} mr={popup?.popupTextMarginRight ?? undefined}>
-            <Box>
-              {/* -------------- popupTitle ----------------*/}
-              {popup?.popupTitle ? (
-                <Box
-                  p={2}
-                  mt={2}
-                  minHeight={popup?.popupTitleHeight ?? undefined}
-                  width={popup?.popupTitleWidth ?? undefined}
-                  textColor={popup?.popupTitleTextColor ?? undefined}
-                >
-                  <Text fontSize="3xl" textAlign={"center"}>
-                    {popup?.popupTitle}
-                  </Text>
-                </Box>
-              ) : null}
+            {popup?.popupContent && (
+              <Box
+                borderColor="black"
+                p={4}
+                minHeight={popup?.popupContentHeight ?? undefined}
+                width={popup?.popupContentWidth ?? undefined}
+                textColor={popup?.popupContentTextColor ?? undefined}
+              >
+                <Text fontWeight="bold">{popup?.popupContent}</Text>
+              </Box>
+            )}
+          </Box>
+        </Flex>
+      )}
 
-              {/* -------------- popupContent --------------*/}
-              {popup?.popupContent ? (
-                <Box
-                  borderColor={"black"}
-                  p={4}
-                  minHeight={popup?.popupContentHeight ?? undefined}
-                  width={popup?.popupContentWidth ?? undefined}
-                  textColor={popup?.popupContentTextColor ?? undefined}
-                >
-                  <Text fontSize="bold">{popup?.popupContent}</Text>
-                </Box>
-              ) : null}
-            </Box>
-          </Flex>
-        )}
-        <Box ref={flexContainerRef} overflowY="scroll"  h="37%" > {/* Apply scrollable overflow to this box */}
-          {/* -------------- PopupChatHistory ----------------*/}
-          {chatGPTs.length > 0 && pastChatGPTInput.length > 0 ? (
-            <Box p={2} m={1}>
-              <Flex direction="column">
-                {chatGPTs.map((chatGPT, index) => (
-                  <Box key={`${chatGPT.requestId}--${chatGPT.id}`}>
-                    <Text
+      <Box ref={flexContainerRef} overflowY="scroll" h={popup?.popupChatHistoryPercentage ?? undefined }>
+          <Box p={2} m={1}>
+            <Flex direction="column">
+              {(chatGPTs.length > 0 ? chatGPTs :  [{id: 0, requestId: 0, inputChatGPT: popup?.popupExampleInputChatGPT, outputChatGPT: popup?.popupExampleOutputChatGPT }]).map((chatGPT, index) => (
+                <Box key={`${chatGPT.requestId}--${chatGPT.id}--${index}`}>
+                  <Flex justifyContent="flex-end">
+                    <Box
                       mt={1}
                       px={2}
                       py={1}
-                      fontSize={"sm"}
-                      borderRadius="md"
-                      borderColor={'white'}
-                      borderWidth={'2px'}
-                      boxShadow="md"
-                      textColor={popup?.popupChatHistoryOutputTextColor ?? undefined}
-                      backgroundColor={popup?.popupChatHistoryOutputBoxColor ?? undefined}
-                      position="relative"
-                      _after={{
-                        content: '""',
-                        position: "absolute",
-                        top: 0,
-                        right: 0,
-                        width: "8px", // Adjust this value as per your preference
-                        height: "8px", // Adjust this value as per your preference
-                        borderTopRightRadius: 0,
-                        backgroundColor: popup?.popupChatHistoryOutputBoxColor ?? undefined,
-                      }}
-                      textAlign="right" // Add this line
-                    >
-                      {pastChatGPTInput[index]}
-                    </Text>
-                    <Text
-                      mt={1}
-                      px={2}
-                      py={1}
-                      fontSize={"sm"}
-                      borderRadius="md"
-                      borderColor={'black'}
-                      borderWidth={'2px'}
+                      fontSize="sm"
+                      borderRadius="8px 8px 0 8px"
+                      borderWidth="1px"
+                      borderColor={popup?.popupChatHistoryInputFocusBorderColor ?? undefined}
                       boxShadow="md"
                       textColor={popup?.popupChatHistoryInputTextColor ?? undefined}
                       backgroundColor={popup?.popupChatHistoryInputBoxColor ?? undefined}
                       position="relative"
+                      _after={{
+                        content: '""',
+                        position: "absolute",
+                        bottom: 0,
+                        right: 0,
+                        width: "8px",
+                        height: "8px",
+                        borderTopRightRadius: 0,
+                        backgroundColor: popup?.popupChatHistoryInputBoxColor ?? undefined,
+                      }}
+                      textAlign={"left" }
+                    >
+                      {chatGPTs.length > 0 ? pastChatGPTInput[index] : popup?.popupExampleInputChatGPT}
+                    </Box>
+                  </Flex>
+                  <Flex justifyContent="flex-start">
+                    <Box
+                      mt={1}
+                      px={2}
+                      py={1}
+                      fontSize="sm"
+                      borderRadius="8px 8px 8px 0"
+                      borderWidth="1px"
+                      borderColor={popup?.popupChatHistoryOutputFocusBorderColor ?? undefined}
+                      boxShadow="md"
+                      textColor={popup?.popupChatHistoryOutputTextColor ?? undefined}
+                      backgroundColor={popup?.popupChatHistoryOutputBoxColor ?? undefined}
+                      position="relative"
                       _before={{
                         content: '""',
                         position: "absolute",
-                        top: 0,
+                        bottom: 0,
                         left: 0,
-                        width: "8px", // Adjust this value as per your preference
-                        height: "8px", // Adjust this value as per your preference
+                        width: "16px",
+                        height: "8px",
                         borderTopLeftRadius: 0,
-                        backgroundColor: popup?.popupChatHistoryInputBoxColor ?? undefined,
+                        backgroundColor: popup?.popupChatHistoryOutputBoxColor ?? undefined,
                       }}
+                      textAlign={"right" }
                     >
-                      {chatGPT.outputChatGPT}
-                    </Text>
-                  </Box>
-                ))}
-              </Flex>
-            </Box>
-          ) : null}
+                      {chatGPT.outputChatGPT }
+                    </Box>
+                  </Flex>
+                </Box>
+              ))}
+            </Flex>
           </Box>
-          <Box ref={flexContainerRef} h="33%" >
-          {chatGPTs.length == 0 ?            
-           <>
-              <Box mx={2}>
+      </Box>
+
+      <Box h={popup?.popupChatSendPercentage ?? undefined}>
+        {chatGPTs.length === 0 && (
+          <>
+            <Box mx={2}>
               {popupAdditionals?.map((suggestion, idx) => (
-                <Button key={suggestion.popupAdditionalId} size="xs" mt={1} ml={1} onClick={() => handleButtonSubmit(suggestion.popupAdditionalText)} >
+                <Button
+                  key={`${suggestion.popupAdditionalId }--${idx}`} 
+                  size="xs"
+                  mt={1}
+                  ml={1}
+                  onClick={() => handleButtonSubmit(suggestion.popupAdditionalText)}
+                >
                   {suggestion.popupAdditionalText}
                 </Button>
               ))}
-
-              </Box>
-            </> : null
-            }
-
-          <Flex>
-            <Box width={"80%"}>
-              <Input
-                type="text"
-                value={inputChatGPT}
-                onChange={handleInputChange}
-                placeholder={popup?.popupChatButtonText ?? "Enter text here"}
-                borderRadius="md"
-                bgColor={popup?.popupChatButtonBoxColor ?? "white"}
-                focusBorderColor={popup?.popupChatButtonTextColor ?? "white"}
-                _placeholder={{ color: "white" }}
-                p={2}
-                m={2}
-              />
             </Box>
-            <Box>
-              <Button onClick={handleSubmit} colorScheme="blue" m={2} ml={3}>
-                {"Send"}
-              </Button>
-            </Box>
-          </Flex>
+          </>
+        )}
+      </Box>
+
+
+        <Box>
+        <Flex>
+          <Box width="80%">
+            <Input
+              type="text"
+              value={inputChatGPT}
+              onChange={handleInputChange}
+              placeholder={popup?.popupChatButtonText ?? "Enter text here"}
+              borderRadius="md"
+              bgColor={popup?.popupChatButtonBoxColor ?? undefined}
+              textColor={popup?.popupChatButtonTextColor ?? undefined}
+              _placeholder={{ color: popup?.popupChatButtonTextColor ?? undefined }}
+              p={2}
+              m={2}
+            />
           </Box>
-        
-      </Box> {/* Closing tag for the innermost Box component */}
-    </Flex>
-  ) : null}
+          <Box>
+            <Button 
+            onClick={handleSubmit} 
+            colorScheme={popup?.popupSendButtonColorScheme ?? undefined} 
+            m={2} 
+            ml={3} 
+            textColor={popup?.popupSendButtonTextColor ?? undefined} 
+            variant = {popup?.popupSendButtonVariant ?? undefined} 
+            color={popup?.popupSendButtonColor ?? undefined} >
+            {popup?.popupSendButtonText ?? 'Send'}
+            </Button>
+          </Box>
+        </Flex>
+        </Box>
+
+    </Box>
+  </Flex>
+)}
+
 </>
 
 
